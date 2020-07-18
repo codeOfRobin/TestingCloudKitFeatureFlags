7e13cb0a100bea8ebe4abc8189306a859cb72023
Robin Malhotra <me@rmalhotra.com>
2020-07-18 23:47:41 +0100

    Testing
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- TestCloudKitFeatureFlags/AppDelegate.swift
+++ TestCloudKitFeatureFlags/AppDelegate.swift
@@ -7,12 +7,18 @@
 
 import Cocoa
 import SwiftUI
+import CloudKitFeatureFlags
+import Combine
+import CloudKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	var window: NSWindow!
 
+	let container = CKContainer(identifier: "iCloud.com.rmalhotra.CloudKitTrial")
+	lazy var featureFlags = CloudKitFeatureFlagsRepository(container: container)
+	var cancellables = Set<AnyCancellable>()
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Create the SwiftUI view that provides the window contents.
@@ -28,6 +34,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		window.setFrameAutosaveName("Main Window")
 		window.contentView = NSHostingView(rootView: contentView)
 		window.makeKeyAndOrderFront(nil)
+
+		featureFlags.featureEnabled(name: "some_feature_flag").sink(receiveCompletion: { (completion) in
+			print(completion)
+		}) { (value) in
+			print(value)
+		}.store(in: &cancellables)
+		createFeatureFlagIfNecessary()
+	}
+
+	func createFeatureFlagIfNecessary() {
+		let featureFlag = FeatureFlag(name: "some_feature_flag", uuid: UUID(), rollout: 0.1, value: true)
+		container.featureFlaggingDatabase.fetch(withRecordID: .init(recordName: "some_feature_flag")) { [weak self] (record, error) in
+			if record == nil {
+				self?.container.featureFlaggingDatabase.save(featureFlag.convertToRecord()) { (record, error) in
+					print(record as Any)
+				}
+			} else {
+				print(record as Any)
+			}
+		}
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
